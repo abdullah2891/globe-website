278dd6c3e3f8b294d3ef8b7d1307fbe693743c5e
abdullah2891 <abdullah2891@gmail.com>
2016-07-03 14:51:57 -0400

    added hover

--- js/app.js
+++ js/app.js
@@ -12,6 +12,13 @@ $(document).ready(function(){
 
  });
 
+
+     $('.dropdown').hover(function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(500);
+    }, function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(500);
+    });
+
 })
 
 
